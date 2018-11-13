d892ffc9734f0501c9a4f518005ae57e2a2193d5
Cartomu Kabba <cartomukabba123@gmail.com>
2018-11-13 22:57:39 +0000

    rpc

--- js/script.js
+++ js/script.js
@@ -9,7 +9,27 @@
 
 // DOCUMENT READY FUNCTION BELOW
 
+var winner = "";
+var randomNumber = Math.random();
+var userChoice = "";
+
 $("#shoot").click(function() {
    $("#userChoice").text($("#input").val()); 
+  $("#computerChoice").text(randomNumber); 
 });
+
+if (randomNumber > .80) {
+    $("#computerChoice").html("rock");
+}else if (randomNumber <.20) {
+    $("#computerChoice").html("paper");
+}else {
+    $("#computerChoice").html("scissors")
+};
+
+
+
+
+
+
+
 
